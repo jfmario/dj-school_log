b9c4088e87eb824662cdf752b4cf0f40e748bc90
John Marion <John Marion>
2016-09-22 01:23:37 -0400

    admin

--- admin.py
+++ admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from school_log.models import Entry, Student, Subject, SubjectToEntry
 
 # Register your models here.
+
+class SubjectToEntryInline ( admin.TabularInline ):
+    model = SubjectToEntry
+    extra = 1
+
+class EntryAdmin ( admin.ModelAdmin ):
+
+    inlines = [SubjectToEntryInline]
+
+    def save_model ( self, request, obj, form, change ):
+        obj.author = request.user
+        obj.save ()
+
+admin.site.register ( Entry, Entry Admin )
+admin.site.register ( Student )
+admin.site.register ( Subject )
