b3b74d5df8e38e5bf4c9cd2b5db02fb5dd688bd5
John Marion <John Marion>
2016-09-24 16:18:24 -0400

    entry.id = None

--- views.py
+++ views.py
@@ -37,7 +37,7 @@ def new_entry ( request ):
             for student_id in request.POST.get ( 'students' ):
 
                 entry = EntryForm ( data=request.POST ).save ( commit=False )
-                entry.pk = None
+                entry.id = None
 
                 student = Student.objects.get ( id=int ( student_id ), user=request.user )
                 entry.student = student
