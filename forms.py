49a4d036dae33858de5596ada0f1c3e072cc30b4
John Marion <John Marion>
2016-09-24 18:42:37 -0400

    student form css

--- forms.py
+++ forms.py
@@ -4,12 +4,7 @@ from django.forms import ModelForm, widgets
 from school_log.models import Entry, Student, Subject
 
 class EntryForm ( ModelForm ):
-    '''
-    def __init__ ( self, user, *args, **kwargs ):
-        super ( EntryForm, self ).__init__ ( *args, **kwargs )
-        # self.fields ['student'] = Student.objects.filter ( user=user )
-        self.fields ['subjects'] = Subject.objects.filter ( user=user )
-    '''
+
     class Meta:
         model = Entry
         fields = [ 'date', 'description', 'hours', 'subjects' ]
@@ -26,6 +21,13 @@ class EntryFormComplete ( ModelForm ):
         fields = [ 'date', 'description', 'hours', 'student', 'subjects' ]
 
 class StudentForm ( ModelForm ):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+        })
     class Meta:
         model = Student
         fields = [ 'name', 'age', 'grade' ]
