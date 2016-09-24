c7bd3ec4c4b33216dd93b9d87afac28dd2fc43ad
John Marion <John Marion>
2016-09-24 16:06:47 -0400

    make entryForm normal again

--- forms.py
+++ forms.py
@@ -4,12 +4,12 @@ from django.forms import ModelForm
 from school_log.models import Entry, Student, Subject
 
 class EntryForm ( ModelForm ):
-
+    '''
     def __init__ ( self, user, *args, **kwargs ):
         super ( EntryForm, self ).__init__ ( *args, **kwargs )
         # self.fields ['student'] = Student.objects.filter ( user=user )
         self.fields ['subjects'] = Subject.objects.filter ( user=user )
-
+    '''
     class Meta:
         model = Entry
         fields = [ 'date', 'description', 'hours', 'subjects' ]
